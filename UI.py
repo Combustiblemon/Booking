2551fecefca662c20c9c32d79f9594b7a2d125c5
Combustiblemon <tasos.hitiris@hotmail.com>
2020-08-04 12:48:48 +0300

    import DB API

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, QtCore, uic
 from PyQt5.QtWidgets import QLabel, QDialog, QMessageBox, QMainWindow, QWidget, QPushButton, QAction, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QIcon
 from PyQt5.uic import uiparser
+import DB
 import os
 import json
 
