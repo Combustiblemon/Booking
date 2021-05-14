a3e4283432c896536a2709126d2756d9a484fdab
Combustiblemon <tasos.hitiris@hotmail.com>
2021-05-14 15:53:16 +0300

    cleanup

--- application/DBUtils.py
+++ application/DBUtils.py
@@ -10,16 +10,48 @@ config = configHandler.getConfig()
 username = None
 password = None
 
-client_id = config['CLIENT']['client_id']
-client_secret = config['CLIENT']['client_secret']
+client_id = None
+client_secret = None
 url = config['CLIENT']['url']
 cert_path = config['CLIENT']['cert_path']
 access_token = None
 
-def getAccessToken() -> str:
+def getClientData():
     global username, password
     while not username or not password:
         username, password = getLoginDetails()
+
+        headers = {
+            'username': username,
+            'password': password,
+        }
+        
+        try:
+            response = requests.get(f'{url}', headers=headers, verify=cert_path)
+            data = response.json()
+            dKeys = [*data]
+            if 'ERROR_USERNAME' in dKeys:
+                username = password = None
+                continue
+            
+            if 'ERROR_PASSWORD' in dKeys:
+                username = password = None
+                continue
+            
+            global client_id, client_secret
+            client_id = data['client_id']
+            client_secret = data['client_secret']
+            return (data['client_id'], data['client_secret'])
+        except requests.exceptions.ConnectionError:
+            w = ErrorWindow('Αποτυχία σύνδεσης με την βάση δεδομένων!')
+            w.show()
+            continue
+
+def getAccessToken() -> str:
+    global client_id, client_secret
+    while not client_id or not client_secret:
+        data = getClientData()
+        
         
     files = {
         'grant_type': (None, 'password'),
@@ -32,13 +64,13 @@ def getAccessToken() -> str:
         response = requests.post(f'{url}oauth/token', files=files, auth=(client_id, client_secret), verify=cert_path)
         data = response.json()
         return data["access_token"]
-    except requests.exceptions.ConnectionError as error:
+    except requests.exceptions.ConnectionError:
         w = ErrorWindow('Αποτυχία σύνδεσης με την βάση δεδομένων!')
         w.show()
 
 def GET(route: str, filters: list):
     global access_token
-    if not access_token:
+    while not access_token:
         access_token = getAccessToken()
    
     headers = {
@@ -51,6 +83,7 @@ def GET(route: str, filters: list):
     response = requests.get(f'{url}api/{route}', json=json.dumps(payload), headers=headers, verify=cert_path)
     if response.status_code == 200:
         return response.json()
+    print(response.text)
     return False
 
 def PUT(route: str, payload: dict):
@@ -69,7 +102,7 @@ def PUT(route: str, payload: dict):
 
 def PATCH(route: str, payload: dict):
     global access_token
-    if not access_token:
+    while not access_token:
         access_token = getAccessToken()
    
     headers = {
@@ -83,7 +116,7 @@ def PATCH(route: str, payload: dict):
 
 def DELETE(route: str, filters: list):
     global access_token
-    if not access_token:
+    while not access_token:
         access_token = getAccessToken()
    
     headers = {
@@ -132,7 +165,7 @@ class ErrorWindow(QtWidgets.QDialog):
         # Load the main UI file
         self.ui = ui_ErrorWindow.Ui_ErrorWindow()
         self.ui.setupUi(self)
-        
+        #self.setWindowFlags(QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowTitle(title)
         
         self.label = self.findChild(QtWidgets.QLabel, 'label')
